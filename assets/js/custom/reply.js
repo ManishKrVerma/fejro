913526cc259451b6cd16a41defc10d8dc0e0d001
ManishKrVerma <manish.verma@devhitech.co.in>
2015-09-14 15:40:40 +0530

    reply.js

--- assets/js/custom/reply.js
+++ assets/js/custom/reply.js
@@ -2,22 +2,12 @@ $(document).ready(function(){
 	$('.reply-model').on('click',function(e){
 		var form_target = $(e.target).parents('form');
 		formtarget = '#'+$(form_target).attr('id');
-		$(formtarget).validate({
-			rules: {
-				comment: {
-					required: true
-				}
-			},
-			messages: {
-				comment: {
-					required: "Please enter something to reply"
-				},
-			},
-			submitHandler: function () {
-				var comment = $(formtarget+' textarea[name=comment]').val();
-				var item = $(formtarget+' input[name=item]').val();
-				return true;
-			}
-		});
+		var comment = $(formtarget+' textarea[name=comment]').val();
+		var item = $(formtarget+' input[name=item]').val();
+		$(formtarget+' #comment-error').remove();
+		if(comment == ''){
+			$(formtarget+' textarea').parent('div').append('<label id="comment-error" class="error" for="comment">Please enter something to reply</label>');
+			return false;
+		}
 	});
 })
